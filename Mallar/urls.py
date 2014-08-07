3ecf8a5346217148e39b14d2f1d263ad1609368e
German <moekko_g@hotmail.com>
2014-08-07 00:26:18 +0000

    DbSynced

--- Mallar/urls.py
+++ Mallar/urls.py
@@ -1,6 +1,9 @@
 from django.conf.urls import patterns, include, url
 
 from django.contrib import admin
+
+from .views import HelloWorldView
+
 admin.autodiscover()
 
 urlpatterns = patterns('',
@@ -8,5 +11,6 @@ urlpatterns = patterns('',
     # url(r'^$', 'Mallar.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
+    url(r'^$', HelloWorldView.as_view(), name='home'),
     url(r'^admin/', include(admin.site.urls)),
 )
--- Region/models.py
+++ Region/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+REGION_TYPES = (
+    ('E', 'Estado'),
+    ('M', 'Municipio'),
+    ('DL', 'Distrito Local'),
+    ('DF', 'Distrito Federal'),
+    ('S', 'Seccion')
+)
+
 
 # Create your models here.
+class Region(models.Model):
+    id_description = models.CharField(max_length=300)
+    description = models.CharField(max_length=300)
+    region_type = models.CharField(max_length=2, default='E', choices=REGION_TYPES)
+
+    def __str__(self):
+        return "{0} - {1}".format(self.id_description, self.description)
+
+class Region_Estructura(models.Model):
+    father_region = models.ForeignKey(Region, related_name="region_father")
+    son_region = models.ForeignKey(Region, related_name="region_son")
